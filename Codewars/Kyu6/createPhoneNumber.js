b7c3fb98ff45ff1b917923df9e919a2039eba3e7
TXCodeMedic <97770102+TXCodeMedic@users.noreply.github.com>
2023-04-25 20:12:55 -0500

    Update createPhoneNumber.js

--- Codewars/Kyu6/createPhoneNumber.js
+++ Codewars/Kyu6/createPhoneNumber.js
@@ -17,3 +17,15 @@ function createPhoneNumber(numbers){
   }
   return first + middle + last
 }
+
+// Function to Learn
+function createPhoneNumber(numbers){
+  var format = "(xxx) xxx-xxxx";
+  
+  for(var i = 0; i < numbers.length; i++)
+  {
+    format = format.replace('x', numbers[i]);
+  }
+  
+  return format;
+}
