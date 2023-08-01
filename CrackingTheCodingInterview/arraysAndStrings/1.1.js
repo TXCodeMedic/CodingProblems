bdb285fe7e45094a5a2c19f800050bd303460049
Matthew Galvez <97770102+TXCodeMedic@users.noreply.github.com>
2023-08-01 07:38:42 -0500

    Update 1.1.js

--- CrackingTheCodingInterview/arraysAndStrings/1.1.js
+++ CrackingTheCodingInterview/arraysAndStrings/1.1.js
@@ -1,1 +1,18 @@
-// Implement an algorithm to determine if a string has all unique characters. What if you cannot use additional data structures?
+// Implement an algorithm to determine if a string has all unique characters.
+
+function allUniqueChars(s) {
+    var mySet = new Set()
+    for (let i = 0; i < s.length; i++){
+    if ( mySet.has(s[i]) ) {
+        return false
+    } else {
+        mySet.add(s[i])
+    }
+}
+    return true
+}
+
+
+// Testing
+console.log(allUniqueChars('abcdefg')) // Output: True
+console.log(allUniqueChars('aaaaabbbb')) // Output: False
