81ddad53d393dd94491aeec05c70e5805e00c3d3
Matthew Galvez <97770102+TXCodeMedic@users.noreply.github.com>
2023-08-07 21:50:19 -0500

    Update addTwoNumbers.js

--- LeetCode/addTwoNumbers.js
+++ LeetCode/addTwoNumbers.js
@@ -44,3 +44,38 @@ var addTwoNumbers = function(l1, l2) {
     }
     return dummy.next
 };
+
+// Second Attempt
+// Runtime: 76ms, Beats 98.36% of JS users
+// Memory: 47.14 mb, Beats 74.88% of JS users
+var addTwoNumbers = function(l1, l2) {
+  var List = new ListNode(0);
+  var head = List;
+  var sum = 0;
+  var carry = 0;
+
+  while(l1 !== null || l2 !== null || sum > 0){
+    if (l1 !== null) {
+      sum += l1.val;
+      l1 = l1.next;
+    }
+    if (l2 !== null) {
+      sum += l2.val;
+      l2 = l2.next;
+    }
+    if (sum >= 10) {
+      carry = 1;
+      sum -= 10;
+    }
+    head.val = sum;
+              
+    if (l1 !== null || l2 !== null || carry > 0) {
+      head.next = new ListNode(carry);
+      head = head.next;
+    }
+
+    sum = carry;
+    carry = 0;
+  }
+return List;
+};
