55924dd27591ebdc17aae48c1cd47607ef2f68ea
Matthew Galvez <97770102+TXCodeMedic@users.noreply.github.com>
2023-08-07 21:51:54 -0500

    Update addTwoNumbers.js

--- LeetCode/addTwoNumbers.js
+++ LeetCode/addTwoNumbers.js
@@ -15,34 +15,31 @@
 // If l1 or l2 is too large, adding numbers together in this fashion will result in a sum using scientific notation
 // which will not allow the linked list to be created correctly
 var addTwoNumbers = function(l1, l2) {
-    var sum1 = ``
-    var sum2 = ``
+  var sum1 = ``
+  var sum2 = ``
 
-    while (l1) {
-        sum1 += l1.val.toString()
-        l1 = l1.next
-    }
-    console.log(sum1)
-    while (l2) {
-        sum2 += l2.val.toString()
-        l2 = l2.next
-    }
-    console.log(sum2)
+  while (l1) {
+    sum1 += l1.val.toString()
+    l1 = l1.next
+  }
+  while (l2) {
+    sum2 += l2.val.toString()
+    l2 = l2.next
+  }
 
-    var sum = parseInt(sum1.split("").reverse().join("")) + parseInt(sum2.split("").reverse().join(""))
-    console.log(sum)
-    sum = sum.toString().split("").reverse()
+  var sum = parseInt(sum1.split("").reverse().join("")) + parseInt(sum2.split("").reverse().join(""))
+  sum = sum.toString().split("").reverse()
 
-    var dummy = new ListNode()
-    var cur = dummy
+  var dummy = new ListNode()
+  var cur = dummy
 
-    for (let i = 0; i < sum.length; i++){
-        newNode = new ListNode()
-        newNode.val = parseInt(sum[i])
-        cur.next = newNode
-        cur = newNode
-    }
-    return dummy.next
+  for (let i = 0; i < sum.length; i++){
+    newNode = new ListNode()
+    newNode.val = parseInt(sum[i])
+    cur.next = newNode
+    cur = newNode
+  }
+return dummy.next
 };
 
 // Second Attempt
