6cec46c74f9197ef7adb3a25da5ef75faaf98ef1
Matthew Galvez <97770102+TXCodeMedic@users.noreply.github.com>
2023-08-14 08:35:19 -0500

    Update kthLargestElementInAnArray.js

--- LeetCode/Medium/kthLargestElementInAnArray.js
+++ LeetCode/Medium/kthLargestElementInAnArray.js
@@ -5,8 +5,8 @@
  */
 
 // First Attempt
-// Runtime: 144 ms, Beats 59.27%
-// Memory: 51.48 mb , Beats 56.67%
+// Runtime: 144 ms, Beats 59.27% of JS users
+// Memory: 51.48 mb , Beats 56.67% of JS users
 
 var findKthLargest = function(nums, k) {
     nums = nums.sort((a, b) => a - b);
@@ -15,4 +15,12 @@ var findKthLargest = function(nums, k) {
         nums.pop()
     }
     return nums[nums.length - 1]
+};
+
+// Second Attempt: popping inst neccessary
+// Runtime: 154 ms, Beats 51.80% of JS users
+// Memory: 51.35 mb, Beats 60.79% of JS users
+var findKthLargest = function(nums, k) {
+    nums = nums.sort((a, b) => a - b);
+    return nums[nums.length - (k)]
 };
