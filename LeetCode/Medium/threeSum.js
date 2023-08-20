cef4ec5c4e0c75f6570229295468bdeaff41e4b2
Matthew Galvez <97770102+TXCodeMedic@users.noreply.github.com>
2023-08-20 10:35:59 -0500

    Update threeSum.js

--- LeetCode/Medium/threeSum.js
+++ LeetCode/Medium/threeSum.js
@@ -1,31 +1,45 @@
-// Fix later 79/312 TC passed
 /**
  * @param {number[]} nums
  * @return {number[][]}
  */
+
+// Second Attempt: Two Pointers
+// Runtime: 141 ms, Beats 95.22% of JS users
+// Memory: 58.88 mb, Beats 68.17% of JS users
 var threeSum = function(nums) {
     var result = [];
+    // Sort the array so that a two pointer solution approach can be used 
     nums = nums.sort((a, b) => a - b);
-    console.log(nums)
-    
+
+    //iterate through the array, and leave room for the two pointers
     for (let i = 0; i < nums.length - 2; i++) {
-        if (i > 0 && nums[i] === nums[i - 1]) continue
+        // Array is sorted left to right, so if nums[i] is greater than 0, there is no possible way to hit 0
+        if (nums[i] > 0) break;
+        // if a repeat is found, skip the index and increase i.
+        if (i > 0 && nums[i] === nums[i - 1]) continue;
+
         else {
-            let left = i + 1;
-            let right = nums.length - 1;
-            console.log('i: ', i, nums[i])
+            // two pointer approach will have left pointer start one index in fron of i and right will be at the end
+            var left = i + 1;
+            var right = nums.length - 1;
+
             while (left < right) {
-                console.log(left, right)
-                let curSum = nums[i] + nums[left] + nums[right];
-                if (curSum < 0) {
-                    left += 1
-                }
-                else if (curSum > 0) {
-                    right -= 1
-                } else {
-                    result.push( [nums[i], nums[left], nums[right] ] );
-                    left += 1
-                }
+                var curSum = nums[i] + nums[left] + nums[right];
+                // if curSum is less than zero, we need to increment left in order to find larger values
+                if (curSum < 0) left++;
+                // if curSum is greater than zero, we need to deincrement right in order to find smaller values
+                else if (curSum > 0) right-- ;
+                    
+                // 0 is acheived
+                else {
+                    result.push([ nums[i], nums[left], nums[right] ])
+                    // skip repeats
+                    while(nums[left] === nums[left + 1]) left++;
+                    while(nums[right] === nums[right - 1]) right--;
+                    // move pointers to see if other pairs with current i is acheiveable
+                    left++;
+                    right--;
+                 }
             }
         }
     }
