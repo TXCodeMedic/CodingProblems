70e5b7cf0d979030b88c8b2abf73b08cf8c69a40
Matthew Galvez <97770102+TXCodeMedic@users.noreply.github.com>
2023-08-25 15:05:41 -0500

    Update findTheIndexOfTheFirstOccurrenceInAString.js

--- LeetCode/Easy/findTheIndexOfTheFirstOccurrenceInAString.js
+++ LeetCode/Easy/findTheIndexOfTheFirstOccurrenceInAString.js
@@ -15,3 +15,18 @@ var strStr = function(haystack, needle) {
     }
     return -1
 };
+
+// Second Attempt: Filter inputs before iterating
+// Runtime: 48 ms, Beats 85.42% of JS users
+// Memory: 41.50 mb, Beats 90.14% of JS users
+var strStr = function(haystack, needle) {
+    if (haystack === needle) { return 0; }
+    // Filter here
+    if (haystack.length < needle.length) return -1;
+    for (let i = 0; i < haystack.length; i++) {
+        if (needle === (haystack.slice(i, i + needle.length))) {
+            return i
+        }
+    }
+    return -1
+};
