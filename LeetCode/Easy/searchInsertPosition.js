4146c8486404a216a55c695afcfa5b930be962fc
Matthew Galvez <97770102+TXCodeMedic@users.noreply.github.com>
2023-08-25 16:08:59 -0500

    Update searchInsertPosition.js

--- LeetCode/Easy/searchInsertPosition.js
+++ LeetCode/Easy/searchInsertPosition.js
@@ -4,8 +4,8 @@
  * @return {number}
  */
 // First Attempt: Binary Search
-// Runtime: 55 ms, Beats 55.05% of JS users
-// Memory: 42.47 mb, Beats 47.52% of JS users
+// Runtime: 52 ms, Beats 69.82% of JS users
+// Memory: 41.70 mb, Beats 90.04% of JS users
 var searchInsert = function(nums, target) {
         var left = 0
         var right = nums.length
