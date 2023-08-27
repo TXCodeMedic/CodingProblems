ff0249cb9b6c2743f59bcbd91597c02b5a60c4db
Matthew Galvez <97770102+TXCodeMedic@users.noreply.github.com>
2023-08-27 16:10:54 -0500

    Update sqrtX.js

--- LeetCode/Easy/sqrtX.js
+++ LeetCode/Easy/sqrtX.js
@@ -8,3 +8,26 @@
 var mySqrt = function(x) {
     return Math.floor(Math.sqrt(x))
 };
+
+// Second Attempt: Without Math Includes Binary Search
+// Runtime: 63 ms, Beats 75.22% of JS users
+// Memory: 43.50 mb, Beats 67.97% of JS users
+var mySqrt = function(x) {
+  var left = 1;
+  var right = Math.floor(x / 2) + 1;
+  var mid;
+
+  while (left <= right) {
+    mid = Math.floor((left + right) / 2);
+
+    if (mid * mid > x) {
+      right = mid - 1;
+    } else if (mid * mid < x) {
+      left = mid + 1;
+    } else {
+      return mid;
+    }
+  }
+
+  return right;
+};
