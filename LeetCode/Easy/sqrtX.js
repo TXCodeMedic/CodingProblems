36237be5967832fc9fed529bc7841643c4fc4656
Matthew Galvez <97770102+TXCodeMedic@users.noreply.github.com>
2023-08-27 16:08:05 -0500

    Create sqrtX.js

---
+++ LeetCode/Easy/sqrtX.js
@@ -0,0 +1,10 @@
+/**
+ * @param {number} x
+ * @return {number}
+ */
+// First Attempt:
+// Runtime: 50 ms, Beats 97.69% of JS users
+// Memory: 44.03 mb, Beats 25.34% of JS users
+var mySqrt = function(x) {
+    return Math.floor(Math.sqrt(x))
+};
