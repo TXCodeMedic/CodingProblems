f1ae2c02c4cefe7f3f39f6918c39a97ae4312605
Matthew Galvez <97770102+TXCodeMedic@users.noreply.github.com>
2023-08-30 20:37:33 -0500

    Create frogJump.js

---
+++ Codility/frogJump.js
@@ -0,0 +1,9 @@
+// you can write to stdout for debugging purposes, e.g.
+// console.log('this is a debug message');
+// Task Score: 100%
+// Correctness: 100%
+// Performance: 100% O(1)
+function solution(X, Y, D) {
+    // Implement your solution here
+    return Math.ceil((Y - X) / D);
+};
