c9cd2b4298fa8bdf98685cee1c087b9c5b5f6e91
Matthew Galvez <97770102+TXCodeMedic@users.noreply.github.com>
2023-09-11 12:09:30 -0500

    Update validSudoku.js

--- LeetCode/Medium/validSudoku.js
+++ LeetCode/Medium/validSudoku.js
@@ -4,32 +4,32 @@
  */
 
 // Runtime: 56 ms, Beats 93.89% of JS users
-// Memory: 44.42 mb, Beats 85.65% of JS users
+// Memory: 44.38 mb, Beats 89.47% of JS users
 var isValidSudoku = function(board) {
     for (let i = 0; i < 9; i++) {
-    let row = new Set(),
-        col = new Set(),
+        let row = new Set();
+        col = new Set();
         box = new Set();
 
-    for (let j = 0; j < 9; j++) {
-      let _row = board[i][j];
-      let _col = board[j][i];
-      let _box = board[3*Math.floor(i/3)+Math.floor(j/3)][3*(i%3)+(j%3)]
+        for (let j = 0; j < 9; j++) {
+          let _row = board[i][j];
+          let _col = board[j][i];
+          let _box = board[3*Math.floor(i/3)+Math.floor(j/3)][3*(i%3)+(j%3)]
       
-      if (_row != '.') {
-        if (row.has(_row)) return false;
-        row.add(_row);
-      }
-      if (_col != '.') {
-        if (col.has(_col)) return false;
-        col.add(_col);
-      }
+          if (_row != '.') {
+          if (row.has(_row)) return false;
+          row.add(_row);
+          }
+          if (_col != '.') {
+            if (col.has(_col)) return false;
+            col.add(_col);
+          }
       
-      if (_box != '.') {
-        if (box.has(_box)) return false;
-        box.add(_box);
-      } 
+          if (_box != '.') {
+            if (box.has(_box)) return false;
+            box.add(_box);
+          } 
+        }
     }
-  }
   return true
 };
