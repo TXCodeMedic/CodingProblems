9ad73629d2705ae789017c4cb522c41db7787546
Matthew Galvez <97770102+TXCodeMedic@users.noreply.github.com>
2023-09-21 08:44:23 -0500

    Create longestConsecutiveSequence.js

---
+++ LeetCode/Medium/longestConsecutiveSequence.js
@@ -0,0 +1,27 @@
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+
+// Runtime: 92 ms, Beats 87.17% of JS users
+// Memory: 61.84 mb, Beats 81.41% of JS users
+var longestConsecutive = function(nums) {
+  if (nums == null || nums.length === 0) { return 0; }
+  
+  const set = new Set(nums);
+  let max = 0;
+
+  for (let num of set) {
+    if (set.has(num - 1)) { continue; }
+
+    let currNum = num;
+    let currMax = 1;
+
+    while (set.has(currNum + 1)) {
+      currNum++;
+      currMax++;
+    }
+    max = Math.max(max, currMax);
+  }
+  return max;
+};
